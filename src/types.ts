0cdb5452daccb49bdfad11e5d0f8bf5cde89a266
Simon he <13917107460@163.com>
2022-07-30 21:43:13 +0800

    chore: update

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export type Merge<A, B> = {
   [K in keyof (A & B)]: K extends keyof B
-  ? B[K]
-  : K extends keyof A
-  ? A[K]
-  : never
+    ? B[K]
+    : K extends keyof A
+      ? A[K]
+      : never
 }
